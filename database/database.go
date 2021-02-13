983666869195f8ec73dd76c8f77afca7b0522334
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-02-13 10:37:21 +0900

    implement db connection

--- database/database.go
+++ database/database.go
@@ -2,17 +2,35 @@ package database
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+func connect() (db *gorm.DB, err error) {
+	dsn := "host=" + os.Getenv("DB_HOST") + " port=5432" + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=disable"
+	return gorm.Open("postgres", dsn)
+}
+
 func Init() {
-	_, err := gorm.Open("postgres", "host=db user=postgres password=password")
+	_, err := connect()
+
 	if err != nil {
-		fmt.Println("connection failed!!!!!!!")
-		fmt.Println("データベースの接続に失敗しました。。。")
+		fmt.Println(" DB connection failed!!!!!!!")
+
+		for i := 0; i < 3; i++ {
+			fmt.Println("retry:" + strconv.Itoa(i+1))
+			time.Sleep(time.Second * 5)
+			_, err := connect()
+			if err == nil {
+				break
+			}
+		}
+
 		return
 	}
-	fmt.Println("DBへの接続には成功したっぽい")
+	fmt.Println("DB connection success!!!!")
 }
